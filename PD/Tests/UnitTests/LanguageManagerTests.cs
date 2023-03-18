fa615c95b12e43701803046bc857960a83ab49de
kwyzgol <32541843+kwyzgol@users.noreply.github.com>
2023-03-18 09:30:37 +0100

    Unit tests added (for the LanguageManager class).

---
+++ PD/Tests/UnitTests/LanguageManagerTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Tests.UnitTests;
+
+[TestFixture]
+public class LanguageManagerTests
+{
+    [Test]
+    public void GetSupportedCultures_GetArray_NotEmpty()
+    {
+        var languageManager = new LanguageManager();
+
+        var result = languageManager.GetSupportedCultures();
+
+        Assert.IsNotEmpty(result);
+    }
+
+    [Test]
+    public void GetLocalizationOptions_GetOptions_NotNull()
+    {
+        var languageManager = new LanguageManager();
+
+        var result = languageManager.GetLocalizationOptions();
+
+        Assert.IsNotNull(result);
+    }
+
+    [Test]
+    public void GetSupportedCultures_English_Exists()
+    {
+        var languageManager = new LanguageManager();
+
+        var result = languageManager.GetSupportedCultures();
+
+        Assert.Contains("en", result);
+    }
+
+    [Test]
+    public void GetSupportedCultures_Polish_Exists()
+    {
+        var languageManager = new LanguageManager();
+
+        var result = languageManager.GetSupportedCultures();
+
+        Assert.Contains("pl", result);
+    }
+}
